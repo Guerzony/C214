46714f4a72cec37b6fb8baab01471afc82048be2
Guerzony <gabrielhenriquecgr2002@gmail.com>
2023-08-28 11:40:57 -0300

    atividade 3

---
+++ atividade3/app_imc/src/site.svelte
@@ -0,0 +1,47 @@
+<script>
+	// Variáveis de estado
+	let peso = 0;      // Peso do usuário
+	let altura = 0;    // Altura do usuário
+	let imc = 0;       // Índice de Massa Corporal
+	let classificacao = '';
+	// Função para calcular o IMC
+	function calcularIMC() {
+	  if (peso > 0 && altura > 0) {
+		imc = (peso / Math.pow(altura, 2)).toFixed(2);
+		if(imc<18.5){
+			classificacao = 'Abaixo do peso'
+
+		}
+		else if(imc >= 18.6 && imc <= 24.9){
+			classificacao = 'Peso ideal(Parabens)'
+		}
+		else if(imc >= 25 && imc <= 29.9){
+			classificacao = 'Levemente acima do peso'
+		}
+		else if(imc >= 30 && imc <= 34.9){
+			classificacao = 'Obesidade grau 1'
+		}
+		else if(imc >= 35 && imc <= 39.9){
+			classificacao = 'Obesidade grau 2 (severa)'
+		}else{
+			classificacao = 'Obesidade grau 3 (mórbida)'
+		}
+	  } else {
+		imc = 0;
+	  }
+	}
+  </script>
+<main>
+	<h1>CLASSIFICAÇÃO IMC </h1>
+	<label>
+	  Peso (kg): <input type="number" bind:value={peso} on:input={calcularIMC} />
+	</label>
+	<label>
+	  Altura (m): <input type="number" bind:value={altura} on:input={calcularIMC} />
+	</label>
+	{#if imc > 0}
+	  <p>Seu IMC é : {imc}</p>
+	  <p>Classificação: {classificacao}</p>
+	{/if}
+  </main>
+
---
+++ atividade3/readme.md
@@ -0,0 +1,7 @@
+#ATIVIDADE 3
+
+Aqui criamos um app para calcular o indice de massa corporal de um usuario com base na tabela de IMC, com o usuario colocando seu peso em kg e sua altura em metros para que o programa mostre qual a condicao de massa corporal do mesmo, sendo normal, abaixo ou acima do peso.
+Utilizou-se o framework svelte e uma logica simples de if e else para o calculo de IMC.
+
+
+
